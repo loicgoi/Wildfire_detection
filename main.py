3811c5fae979c8dc92333a2bbaacbd0f4c77f1d5
[loicgoi] <g.loic.cours@gmail.com>
2025-09-22 16:54:27 +0200

    feature split dataset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from pipeline import run_pipeline
+from prepare_data.split_dataset import split_dataset
 
 if __name__ == "__main__":
     json_path = Path("data/satellite_wildfire_detection/_annotations.coco.json")
@@ -7,4 +8,8 @@ if __name__ == "__main__":
         "data/satellite_wildfire_detection/_annotations_cleaned.coco.json"
     )
 
+    # On lance le pipeline de traintement
     run_pipeline(json_path, json_out_path)
+
+    # On lance le split des données pour l'entrainement
+    split_dataset()
