a477a9a608cd346d78b76e9291cfc4de043dc74b
[loicgoi] <g.loic.cours@gmail.com>
2025-09-22 12:18:43 +0200

    add visualize.py

---
+++ visualize.py
@@ -0,0 +1,17 @@
+import fiftyone as fo
+from pathlib import Path
+
+
+dataset_dir = Path("data/satellite_wildfire_detection")
+coco_json = Path("data/satellite_wildfire_detection/_annotations_cleaned.coco.json")
+
+dataset = fo.Dataset.from_dir(
+    dataset_dir=".",
+    dataset_type=fo.types.COCODetectionDataset,
+    labels_path=coco_json,
+    name="wildfire_dataset",
+    data_path="data/satellite_wildfire_detection",
+)
+
+session = fo.launch_app(dataset, remote=True, port=5151)
+session.wait()
